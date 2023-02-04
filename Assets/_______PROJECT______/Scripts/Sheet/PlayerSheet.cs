f9d91b662f2420bf24a5a1556e47276b8858725c
Buannic Arthur <arthur.buannic@musicworldmedia.com>
2023-02-04 10:11:39 +0100

    [dev] Slight code cleaning

--- Assets/_______PROJECT______/Scripts/Sheet/PlayerSheet.cs
+++ Assets/_______PROJECT______/Scripts/Sheet/PlayerSheet.cs
@@ -5,10 +5,27 @@ public class PlayerSheet {
     public Dictionary<PlayerStats, int> Stats;
     public Dictionary<ItemSlot, Item> Equipment;
 
+#region Initialization
+
     public PlayerSheet() {
-        Stats = new Dictionary<PlayerStats, int>();
+        Stats = GetDefaultStats();
         Equipment = new Dictionary<ItemSlot, Item>();
     }
+
+    private Dictionary<PlayerStats, int> GetDefaultStats() {
+        return new Dictionary<PlayerStats, int> {
+            {PlayerStats.Strength, 1},
+            {PlayerStats.MagicPower, 1},
+            {PlayerStats.AttackSpeed, 1},
+            {PlayerStats.MovementSpeed, 1},
+            {PlayerStats.Defense, 1},
+            {PlayerStats.MaxHp, 10}
+        };
+    }
+
+#endregion
+
+#region Inventory
 
     public void Equip(ItemSlot slot, Item item) {
         if (Equipment.ContainsKey(slot)) {
@@ -28,6 +45,8 @@ public class PlayerSheet {
         Equipment[slot] = null;
         return dropped;
     }
+
+#endregion
 
     private void RefreshStats() {
         foreach (var kvp in Stats) {
