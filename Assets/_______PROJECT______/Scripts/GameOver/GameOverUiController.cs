8af05c271a1b6a5429cf6276cc9573b484d7b36a
Clement <clement.laurent@musicworldmedia.com>
2023-02-05 00:55:18 +0100

    [UI] Improved game over

--- Assets/_______PROJECT______/Scripts/GameOver/GameOverUiController.cs
+++ Assets/_______PROJECT______/Scripts/GameOver/GameOverUiController.cs
@@ -1,14 +1,40 @@
+using System;
 using System.Collections.Generic;
+using Coffee.UIEffects;
 using DG.Tweening;
+using Sirenix.OdinInspector;
+using TMPro;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class GameOverUiController : MonoBehaviour {
 
     [SerializeField] private CanvasGroup _group;
 
-    public void Appear(List<AncestorData> victories) {
+
+    [SerializeField] private AnimationCurve _valueCurve;
+    [SerializeField] private UIHsvModifier _hsvModifier;
+    [SerializeField] private float _speed;
+    [SerializeField] private Vector2 _valueRange;
+    
+    [SerializeField] private List<string> _messages;
+    [SerializeField] private TextMeshProUGUI _text;
+    private void Update()
+    {
+        _hsvModifier.value = Mathf.Lerp(_valueRange.x, _valueRange.y,_valueCurve.Evaluate(Time.time*_speed));
+    }
+
+    [Button]
+    public void Appear(List<AncestorData> victories)
+    {
+        _text.text = "";
+        _text.color = new Color(1f,1f,1f,0f);
+        
         var gameOverFadeIn = DOTween.Sequence();
         gameOverFadeIn.Append(_group.DOFade(1, 0.3f));
+        gameOverFadeIn.Append(_text.DOFade(1, 1f));
+        gameOverFadeIn.Join(_text.DOText(_messages[Random.Range(0, _messages.Count - 1)], 1f));
+        
     }
 
 }
