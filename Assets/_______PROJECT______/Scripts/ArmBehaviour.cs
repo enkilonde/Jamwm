eef9324f86b24f68629b24a5c7d3f7c2a1545c83
enki_londe <enkilonde@gmail.com>
2023-02-04 13:06:27 +0100

    Arm race

---
+++ Assets/_______PROJECT______/Scripts/ArmBehaviour.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmBehaviour : MonoBehaviour
+{
+    private Transform originalParent;
+    private Vector3 originalPosition;
+    private Vector3 wantedPosition => originalParent.position + originalPosition;
+
+    public float moveSpeed;
+    public float moveSpeedSpring;
+    private float currentSpeed;
+
+    public float rotationSpeed;
+
+    private void Awake()
+    {
+        originalParent= transform.parent;
+        originalPosition = transform.localPosition;
+        transform.parent = null;
+    }
+
+    private void Update()
+    {
+        float dist = Vector3.Distance(transform.position, wantedPosition);
+
+        currentSpeed = Mathf.Lerp(currentSpeed, dist * moveSpeed, Time.deltaTime * moveSpeedSpring);
+
+        transform.position += (wantedPosition - transform.position).normalized * Time.deltaTime * currentSpeed;
+        //transform.position = Vector3.MoveTowards(transform.position, wantedPosition, Time.deltaTime * currentSpeed);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, originalParent.rotation, Time.deltaTime * rotationSpeed);
+
+    }
+}
