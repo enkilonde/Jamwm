5f39d1bd9ce18a09a91ae243a247b459d8191154
enki_londe <enkilonde@gmail.com>
2023-02-05 15:28:26 +0100

    f

--- Assets/_______PROJECT______/Scripts/CustomCharacterController.cs
+++ Assets/_______PROJECT______/Scripts/CustomCharacterController.cs
@@ -114,10 +114,6 @@ public class CustomCharacterController : MonoBehaviour
     public void Move(Vector3 direction)
     {
         Move(new Vector2(direction.x, direction.z));
-
-        direction = transform.TransformDirection(direction);
-        playerVisual.bodyAnimator.SetFloat("WalkDirectionX", direction.x);
-        playerVisual.bodyAnimator.SetFloat("WalkDirectionY", direction.z);
     }
 
     public void Move(Vector2 direction)
@@ -140,6 +136,10 @@ public class CustomCharacterController : MonoBehaviour
 
         characterController.Move(lastMovingDirection * effectiveMoveSpeed * Time.deltaTime);
         if (transform.position.y > 0) characterController.Move(-transform.up * transform.position.y);
+
+        Vector3 dir = transform.TransformDirection(lastMovingDirection);
+        playerVisual.bodyAnimator.SetFloat("WalkDirectionX", dir.x);
+        playerVisual.bodyAnimator.SetFloat("WalkDirectionY", dir.z);
     }
 
     public void Turn(Vector2 vector2)
