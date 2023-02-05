7ffd25b5b2350c48f3196a2c5b860793438fcfea
enki_londe <enkilonde@gmail.com>
2023-02-05 15:57:12 +0100

    rqgrtb

--- Assets/_______PROJECT______/Scripts/Ancestors/AncestorData.cs
+++ Assets/_______PROJECT______/Scripts/Ancestors/AncestorData.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 public class AncestorData {
 
     public readonly string Name;
@@ -6,18 +8,19 @@ public class AncestorData {
 
     private BossSheet _sheet;
 
+    public Dictionary<PlayerStats, int> stats;
+    public Dictionary<ItemSlot, Item> equipment;
+
     public AncestorData(string name, int level) {
         Name = name;
         Level = level;
+        stats = AncestorGenerator.Instance.GenerateBossStats(Level);
+        equipment = AncestorGenerator.Instance.GenerateBossEquipment(Level);
     }
 
     public BossSheet GetDetailedSheet(CustomCharacterController boss) {
         if (_sheet == null) {
-            _sheet = new BossSheet(
-                boss : boss,
-                stats: AncestorGenerator.Instance.GenerateBossStats(Level),
-                equipment: AncestorGenerator.Instance.GenerateBossEquipment(Level)
-            );
+            _sheet = new BossSheet(boss, stats, equipment);
         }
 
         return _sheet;
