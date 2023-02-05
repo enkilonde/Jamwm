ac9de9fefe3e0050405a434b25c324f03513c3ac
Clement <clement.laurent@musicworldmedia.com>
2023-02-05 12:51:00 +0100

    [SFX][INPUT] added sound and input

--- Assets/_______PROJECT______/Scripts/UI/UIMenuManager.cs
+++ Assets/_______PROJECT______/Scripts/UI/UIMenuManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using DG.Tweening;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
@@ -22,7 +23,12 @@ public class UIMenuManager : MonoBehaviour
     [SerializeField] private RectTransform _cursor;
     [SerializeField] private Image _cursorGlow;
 
+    [SerializeField] private List<Button> _buttons;
+
     private float _targetCursor;
+    private int _currentButton =0;
+    private bool _changedButton =false;
+    private float _vertical;
 
     private void Start()
     {
@@ -31,8 +37,6 @@ public class UIMenuManager : MonoBehaviour
         SoundManager.INSTANCE.SetMusicType(SoundManager.MusicType.Home);
         SoundManager.INSTANCE.SetMusicState(SoundManager.MusicState.Playing);
    
-        // _playButton.OnPointerEnter(() => { Play();});
-        
         _playButton.onClick.AddListener(Play);
         _quitButton.onClick.AddListener(Quit);
 
@@ -43,13 +47,46 @@ public class UIMenuManager : MonoBehaviour
         _logo.DORotate(new Vector3(0f, 0f, 10f), 5f).SetLoops(-1, LoopType.Yoyo).SetEase(Ease.InOutSine);
 
         _cursorGlow.DOFade(.2f,1f).SetEase(Ease.InOutSine).SetLoops(-1, LoopType.Yoyo);
-
-        _targetCursor = _playButton.GetComponent<RectTransform>().anchoredPosition.y;
+        
+        SelectButton(_buttons[_currentButton]);
+    }
+    
+    public void Move(InputAction.CallbackContext context)
+    {
+        _vertical = context.ReadValue<Vector2>().y;
     }
 
     private void Update()
     {
-        _cursor.anchoredPosition = Vector2.Lerp(_cursor.anchoredPosition,  new Vector2(0f, _targetCursor), 0.1f);
+        _cursor.anchoredPosition = Vector2.Lerp(_cursor.anchoredPosition,  new Vector2(0f, _targetCursor), 0.3f);
+        
+        if (_vertical > 0.2f && _changedButton == false)
+        {
+            _currentButton--;
+            if (_currentButton<0)
+            {
+                _currentButton = _buttons.Count - 1;
+            }
+            _changedButton = true;
+            SelectButton(_buttons[_currentButton]);
+        }
+        else if (_vertical < -0.2f && _changedButton == false)
+        {
+            _currentButton++;
+            if (_currentButton>=_buttons.Count)
+            {
+                _currentButton = 0;
+            }
+            _changedButton = true;
+            SelectButton(_buttons[_currentButton]);
+        }
+        else if (Mathf.Abs(_vertical)<0.2f)
+        {
+            if (_changedButton)
+            {
+                _changedButton = false;
+            }
+        }
     }
 
     public void SelectButton(Button button)
@@ -60,8 +97,8 @@ public class UIMenuManager : MonoBehaviour
         _targetCursor = button.GetComponent<RectTransform>().anchoredPosition.y;
         
         _logoDetail.DOShakeAnchorPos(0.3f, 3f);
-
     }
+    
     
     public void Play()
     {
@@ -75,4 +112,9 @@ public class UIMenuManager : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void Select()
+    {
+        _buttons[_currentButton].onClick.Invoke();
+}
 }
